671e92087d65e8ccc67da16da435a785b02c1c95
Grant Jenks <grant.jenks@gmail.com>
2021-02-07 22:21:33 -0800

    Add tests for config parsing
    Supported configs:
    
    - .blue
    - setup.cfg
    - tox.ini
    - pyproject.toml
    
    Flake8 will also parse configs in the user's home directory but that is not
    tested.

---
+++ tests/config_blue/example.py
@@ -0,0 +1,4 @@
+def example():
+    return (
+        x1 + x2 + x3 + x4
+    )
--- tests/test_blue.py
+++ tests/test_blue.py
@@ -1,5 +1,22 @@
+import pathlib
+import subprocess
+
 import blue
+import pytest
+
+tests_dir = pathlib.Path(__file__).parent.absolute()
 
 
 def test_version():
     assert blue.__version__
+
+
+@pytest.mark.parametrize(
+    'config_dir',
+    ['config_setup', 'config_tox', 'config_blue', 'config_pyproject'],
+)
+def test_configs(monkeypatch, config_dir):
+    config_dir = tests_dir / config_dir
+    monkeypatch.chdir(config_dir)
+    proc = subprocess.run(f'blue --check .', shell=True)
+    assert proc.returncode == 0
