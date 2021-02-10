faf30bff1c3dbdc5fd9ee163048780cfda964103
Grant Jenks <grant.jenks@gmail.com>
2021-02-09 22:44:55 -0800

    Fix --version option handling and add test cases
    * Previous changes (GH#30) broke --version option handling
    * Simplify --version option handling
    * Add test case for "good" demo code
    * Add test case for "bad" demo code
    * Add test case for __main__.py
    * Add test case for --version option
    * Require coverage at 87%

--- tests/test_blue.py
+++ tests/test_blue.py
@@ -1,22 +1,60 @@
 import pathlib
 import subprocess
 
+import black
 import blue
 import pytest
 
 tests_dir = pathlib.Path(__file__).parent.absolute()
 
 
-def test_version():
-    assert blue.__version__
+@pytest.mark.parametrize(
+    'test_dir',
+    [
+        'config_setup',
+        'config_tox',
+        'config_blue',
+        'config_pyproject',
+        'good_cases',
+    ],
+)
+def test_good_dirs(monkeypatch, test_dir):
+    test_dir = tests_dir / test_dir
+    monkeypatch.chdir(test_dir)
+    monkeypatch.setattr('sys.argv', ['blue', '--check', '.'])
+    black.find_project_root.cache_clear()
+    with pytest.raises(SystemExit) as exc_info:
+        blue.main()
+    assert exc_info.value.code == 0
 
 
 @pytest.mark.parametrize(
-    'config_dir',
-    ['config_setup', 'config_tox', 'config_blue', 'config_pyproject'],
+    'test_dir',
+    ['bad_cases'],
 )
-def test_configs(monkeypatch, config_dir):
-    config_dir = tests_dir / config_dir
-    monkeypatch.chdir(config_dir)
-    proc = subprocess.run(f'blue --check .', shell=True)
-    assert proc.returncode == 0
+def test_bad_dirs(monkeypatch, test_dir):
+    test_dir = tests_dir / test_dir
+    monkeypatch.chdir(test_dir)
+    monkeypatch.setattr('sys.argv', ['blue', '--check', '.'])
+    black.find_project_root.cache_clear()
+    with pytest.raises(SystemExit) as exc_info:
+        blue.main()
+    assert exc_info.value.code == 1
+
+
+def test_main(capsys, monkeypatch):
+    monkeypatch.setattr('sys.argv', ['blue', '--version'])
+    with pytest.raises(SystemExit) as exc_info:
+        import blue.__main__
+    assert exc_info.value.code == 0
+
+
+def test_version(capsys, monkeypatch):
+    monkeypatch.setattr('sys.argv', ['blue', '--version'])
+    with pytest.raises(SystemExit) as exc_info:
+        blue.main()
+    assert exc_info.value.code == 0
+    out, err = capsys.readouterr()
+    version = f'blue, version {blue.__version__}, based on black {black.__version__}\n'
+    assert out == version
+    assert err == ''
