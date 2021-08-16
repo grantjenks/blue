ba3123924739daf6e7a871ede1e4063a99f7cbee
Grant Jenks <grant.jenks@gmail.com>
2021-08-15 19:50:41 -0700

    Add docstrings with newlines.

--- tests/good_cases/tdq_docstrings.py
+++ tests/good_cases/tdq_docstrings.py
@@ -1,16 +1,32 @@
 """This is a module docstring.
 
 Test triple-double-quotes docstrings.
-
 """
 
 
-class Test:
+class Test1:
     """This is a class docstring."""
 
     pass
 
 
-def test():
+class Test2:
+    """This is a class docstring.
+
+    And there's more.
+    """
+
+    pass
+
+
+def test1():
     """This is a func docstring."""
+    pass
+
+
+def test2():
+    """This is a func docstring.
+
+    And there's more.
+    """
     pass
