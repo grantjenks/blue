94d04e48597c2cb5fb43c50949b9763a7a8ea590
Barry Warsaw <barry@python.org>
2022-05-01 15:10:43 -0600

    Additional post-CI fix changes
    * Change the import spellings to produce shorter lines and thus avoid tripping
      up flake8.
    * Ignore E402 from flake8 since the black imports have to happen after the
      import machinery hacks.
    * Several changes to the test_blue.py test suite:
      - Rather than touching the files in the source directory, it creates a
        tempdir and copies the files to bluecheck into that tempdir.

--- tests/test_blue.py
+++ tests/test_blue.py
@@ -1,10 +1,15 @@
 import asyncio
 import pathlib
 
+# blue must be imported before black.  See GH#72.
 import blue
-
 import black
 import pytest
+
+from contextlib import ExitStack
+from shutil import copy
+from tempfile import TemporaryDirectory
+
 
 tests_dir = pathlib.Path(__file__).parent.absolute()
 
@@ -19,34 +24,43 @@ tests_dir = pathlib.Path(__file__).parent.absolute()
         'good_cases',
     ],
 )
-def test_good_dirs(monkeypatch, test_dir):
-    test_dir = tests_dir / test_dir
-    monkeypatch.chdir(test_dir)
+def test_good_dirs(monkeypatch, capsys, test_dir):
+    src_dir = tests_dir / test_dir
     monkeypatch.setattr('sys.argv', ['blue', '--check', '--diff', '.'])
-    for path in test_dir.rglob('*'):
-        path.touch()  # Invalidate file caches in Blue.
-    black.find_project_root.cache_clear()
-    with pytest.raises(SystemExit) as exc_info:
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        blue.main()
-    assert exc_info.value.code == 0
+    with TemporaryDirectory() as dst_dir:
+        # warsaw(2022-05-01): we can't use shutil.copytree() here until we
+        # drop Python 3.7 support because we need dirs_exist_ok and that was
+        # added in Python 3.8
+        for path in src_dir.rglob('*'):
+            copy(src_dir / path, dst_dir)
+        monkeypatch.chdir(dst_dir)
+        black.find_project_root.cache_clear()
+        with pytest.raises(SystemExit) as exc_info:
+            asyncio.set_event_loop(asyncio.new_event_loop())
+            blue.main()
+        assert exc_info.value.code == 0
 
 
 @pytest.mark.parametrize(
     'test_dir',
     ['bad_cases'],
 )
-def test_bad_dirs(monkeypatch, test_dir):
-    test_dir = tests_dir / test_dir
-    monkeypatch.chdir(test_dir)
+def test_bad_dirs(monkeypatch, capsys, test_dir):
+    src_dir = tests_dir / test_dir
     monkeypatch.setattr('sys.argv', ['blue', '--check', '--diff', '.'])
-    for path in test_dir.rglob('*'):
-        path.touch()  # Invalidate file caches in Blue.
-    black.find_project_root.cache_clear()
-    with pytest.raises(SystemExit) as exc_info:
+    with ExitStack() as resources:
+        dst_dir = resources.enter_context(TemporaryDirectory())
+        # warsaw(2022-05-01): we can't use shutil.copytree() here until we
+        # drop Python 3.7 support because we need dirs_exist_ok and that was
+        # added in Python 3.8
+        for path in src_dir.rglob('*'):
+            copy(src_dir / path, dst_dir)
+        monkeypatch.chdir(dst_dir)
+        black.find_project_root.cache_clear()
+        exc_info = resources.enter_context(pytest.raises(SystemExit))
         asyncio.set_event_loop(asyncio.new_event_loop())
         blue.main()
-    assert exc_info.value.code == 1
+        assert exc_info.value.code == 1
 
 
 def test_main(capsys, monkeypatch):
